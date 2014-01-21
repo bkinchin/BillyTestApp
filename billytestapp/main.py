2bc1492374d55840264a21ea3faf3de70c324202
Billy Kinchin <bkinchin@gmail.com>
2014-01-21 16:49:39 +1100

    Working html5 player

--- billytestapp/main.py
+++ billytestapp/main.py
@@ -14,11 +14,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import urllib
+
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #self.response.write('Hello world!')
+        greetings = 'hello Billy'
+        template_values = {
+            'templateVar': greetings,
+        }
+        
+        template = JINJA_ENVIRONMENT.get_template('/googleads-ima-html5/simple/index.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
